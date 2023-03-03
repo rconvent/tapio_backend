8b169cef0d6d1eb53049a6b46471ff502987c5a3
raphael convent <raphael@youmeal.io>
2023-03-03 15:41:02 +0100

    remove

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6!4y%movla1^#34!#&^jd!htt9hhw4qxzwqorxzu-349e=m80('
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
